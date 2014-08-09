06236e27687ab851c0e7fa43b4b04cb1a102d43a
jgerardsimcock <justin_simcock@yahoo.com>
2014-08-09 11:30:54 -1000

    first pass

--- challenge_4.js
+++ challenge_4.js
@@ -11,13 +11,29 @@ exports.largestPalindromeProduct = function(multiplicands, digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
-  // do your work here
+  function largestPalindrome(){
+        var i; //F1
+        var j; //F2
+        var product = i*j; //product of F1*F2
+        var productArray = [];
+        for(i = 1; i < 9; i++) { //looping through F1
+          for(j = 1; j < 9; j++){//looping through F2
+            productArray.push(product.toString());
+          }
+        }
 
-
-  
-  return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
-  };
+        for(var k = 0; k < productArray.length; k++) {
+          if (productArray[k].reverse() === productArray[k]) {
+             //if there are more than one values that are palindromes 
+          }
+                
+        //if array.length is longer than 1 value find the highest value
+        //return F1, F2 and palindrome#
+  }
+       
+  // return {
+  //   factor_0 : factor_0,
+  //   factor_1 : factor_1,
+  //   palindromeNumber : palindromeNumber
+  // };
 };
